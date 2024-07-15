88c016f40a48d70a03e09bce93184eaf2903ba2c
JacksonSRodrigues <jackson.sunny.rodrigues@gmail.com>
2024-07-15 23:53:36 +0530

    fix failing test cases

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import mockAxios from "jest-mock-axios";
 
-test('renders learn react link', () => {
+jest.mock("react-router-dom", () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual("react-router-dom");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    // add your noops here
+    useParams: jest.fn(),
+    useHistory: jest.fn(),
+  };
+});
+
+afterEach(() => {
+  // cleaning up the mess left behind the previous test
+  mockAxios.reset();
+});
+
+test("renders learn react link", () => {
+  mockAxios.get.mockResolvedValue({ data: "test response" });
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
